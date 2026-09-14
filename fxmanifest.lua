61e2260ffdfce7b81c815c5921edb44981856331
jppedrotagawa-ship-it <jppedrotagawa@gmail.com>
2026-09-13 23:26:34 -0300

    fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
 
+author 'Hyper Development'
+description 'Hyper Menu - FiveM Framework'
+version '3.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/assets/**/*'
+}
+
+client_scripts {
+    'config.lua',
+    'client/main.lua',
+    'client/peds.lua',
+    'client/vehicles.lua',
+    'client/pvp.lua'
+}
+
+server_scripts {
+    'server/main.lua',
+    'server/exploits.lua'
+}
