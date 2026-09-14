76bcc302259a48b2ffe48e65edc17f30e3a31c7d
jppedrotagawa-ship-it <jppedrotagawa@gmail.com>
2026-09-13 23:48:42 -0300

    config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,17 @@
+Config = {}
 
+Config.MenuSettings = {
+    Keybind = 'SCROLLLOCK',
+    Theme = 'Hyper',
+    ColorSchema = 'Normal (Cinza)',
+    BlackBackground = true,
+    Watermark = true,
+    SafeMode = false
+}
+
+Config.BypassSettings = {
+    AimbotExecutorBypass = true,
+    MagicBulletBypass = true,
+    AutoPauseAdmin = true,
+    AdminDistanceAlert = 150.0
+}
